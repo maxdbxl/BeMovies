bd005c6f7509cd1c54e094480d3a92ab9a2b2d4b
Rashy-hub <yagoubi.r@gmail.com>
2024-06-20 14:42:46 +0200

    refacto : SwiperFactory function

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,86 +1,54 @@
-const swiper1 = new Swiper('.swiper1',  {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    breakpoints: {
-      500:{
-          slidesPerView:2,
-      },
-      768:{
-          slidesPerView:3,
-      },
-      1250: {
-          slidesPerView:4,
-      },
-  },
-    centeredSlides: false,
-    slidesOffsetAfter: 100,
-    slidesOffsetbefore: 100,
-    spaceBetween: 20,
-    slidesPerGroup: 1,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next1',
-      prevEl: '.swiper-button-prev1',
-    },
+//Swiper events handler
 
-  });
-  
-  const swiper2 = new Swiper('.swiper2',  {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    breakpoints: {
-      500:{
-          slidesPerView:2,
-      },
-      768:{
-          slidesPerView:3,
-      },
-      1250: {
-          slidesPerView:4,
-      },
-  },
-    centeredSlides: false,
-    slidesOffsetAfter: 100,
-    slidesOffsetbefore: 100,
-    spaceBetween: 20,
-    slidesPerGroup: 1,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next2',
-      prevEl: '.swiper-button-prev2',
-    },
+const swiperOnInit = function (event) {
+    if (event.el.classList.contains('swiper1')) {
+        console.log('swiper initialized in results section')
+    } else if (event.el.classList.contains('swiper2')) {
+        console.log('swiper initialized in latest section')
+        //load default latest images from movie database
+    } else if (event.el.classList.contains('swiper3')) {
+        console.log('swiper initialized in genre')
+        //load default active genre images from movie database
+    } else console.log('unknown swiper module : default images loaded')
+}
 
-  });
+// Swiper factory that helps create multiple independant swipers
+function SwiperFactory(containerClass, buttonsClass) {
+    const swiper = new Swiper(containerClass, {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+        breakpoints: {
+            500: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1250: {
+                slidesPerView: 4,
+            },
+        },
+        slidesOffsetAfter: 100,
+        slidesOffsetbefore: 100,
+        //setWrapperSize: true,
 
-  const swiper3 = new Swiper('.swiper3',  {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    breakpoints: {
-      500:{
-          slidesPerView:2,
-      },
-      768:{
-          slidesPerView:3,
-      },
-      1250: {
-          slidesPerView:4,
-      },
-  },
-    centeredSlides: false,
-    slidesOffsetAfter: 100,
-    slidesOffsetbefore: 100,
-    spaceBetween: 20,
-    slidesPerGroup: 1,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next3',
-      prevEl: '.swiper-button-prev3',
-    },
+        // Navigation arrows with unique class names
+        navigation: {
+            nextEl: `.swiper-button-next${buttonsClass}`,
+            prevEl: `.swiper-button-prev${buttonsClass}`,
+        },
 
-  });
+        on: {
+            init: swiperOnInit,
+        },
+    })
+    return swiper
+}
+
+const swiper1 = SwiperFactory('.swiper1', '1')
+const swiper2 = SwiperFactory('.swiper2', '2')
+const swiper3 = SwiperFactory('.swiper3', '3')

7b905864289453fa8672759f787867a629cbe547
Rashy-hub <yagoubi.r@gmail.com>
2024-06-30 11:15:38 +0200

    fix : pagination and loading on demand now really working fine :)

--- Javascript/app.js
+++ Javascript/app.js
@@ -10,7 +10,7 @@ resultsSection.style.display = 'none'
 let resultsPagination = {
     totalPage: 0,
     actualPage: 0,
-    totalCount: 0,
+    totalCount: 4,
     lastSearchInput: '',
 }
 
@@ -22,6 +22,8 @@ const searchSubmitHandler = (event) => {
         resultsSection.style.display = 'none'
         return
     } else {
+        console.log('searching for ' + searchInput.value)
+        // resultsPagination.totalCount = 0
         resultFetchData(searchInput.value, 1)
 
         resultsSection.style.display = 'flex'
@@ -35,14 +37,10 @@ const searchSubmitHandler = (event) => {
 
 //Swiper events handler
 
-const navigationNextHandler = function (swiper) {}
-
-const navigationPrevHandler = function (swiper) {}
-
 const loadMoreHandler = function (swiper) {
-    console.log('load more pictures from here')
+    console.log('load more pictures from here  :')
     resultsPagination.actualPage++
-    console.log(resultsPagination)
+
     if (resultsPagination.actualPage > resultsPagination.totalPage)
         console.log('No mores pages to load')
     else {
@@ -94,38 +92,45 @@ function SwiperFactory(containerClass, buttonsClass) {
             nextEl: `.swiper-button-next${buttonsClass}`,
             prevEl: `.swiper-button-prev${buttonsClass}`,
         },
-
+        grabCursor: true,
         on: {
             init: swiperOnInit,
-            navigationNext: navigationNextHandler,
-            navigationPrev: navigationPrevHandler,
-            reachEnd: loadMoreHandler,
         },
     })
     return swiper
 }
 
 function initResultImage(results) {
-    console.log(results.length)
+    swiper1.slideTo(0, 1, false)
+    swiper1.off('reachEnd', loadMoreHandler)
+    swiper1.removeAllSlides()
     for (let index = 0; index < results.length; index++) {
-        const slide = `<div class="swiper-slide"><img class="resultimages" src="https://image.tmdb.org/t/p/original/${results[index].poster_path}" loading="lazy" alt=""/> <div class="swiper-lazy-preloader"></div></div>`
-        if (index < 4) swiper1.removeSlide(0)
+        console.log(`creating ${results.length} new slide : `)
+        const slide = `<div class="swiper-slide"><img class="resultimages" src="https://image.tmdb.org/t/p/original/${results[index].poster_path}" loading="lazy" alt=""/>   <div class="swiper-lazy-preloader"></div> </div>`
+
         swiper1.appendSlide(slide)
         swiper1.update()
     }
+    swiper1.on('reachEnd', loadMoreHandler)
+    resultsPagination.totalCount = 0
+    resultsPagination.totalCount += results.length
+    console.log(
+        `totalCount of firstPage equal to ${resultsPagination.totalCount}`
+    )
 }
 
 function updateResultImage(results) {
-    console.log(results.length)
+    console.log(`swiper UPDATE with ${results.length} images`)
     for (let index = 0; index < results.length; index++) {
-        const slide = `<div class="swiper-slide"><img class="resultimages" src="https://image.tmdb.org/t/p/original/${results[index].poster_path}" loading="lazy" alt=""/> <div class="swiper-lazy-preloader"></div></div>`
+        const slide = `<div class="swiper-slide"><img class="resultimages" src="https://image.tmdb.org/t/p/original/${results[index].poster_path}" loading="lazy" alt=""/>   <div class="swiper-lazy-preloader"></div> </div>`
 
         swiper1.appendSlide(slide)
-        swiper1.update()
     }
+    resultsPagination.totalCount += results.length
+    console.log(`totalCount now equal to ${resultsPagination.totalCount}`)
 }
-//Call Api handler
-function resultFetchData(searchValue, page) {
+
+async function resultFetchData(searchValue, page) {
     const options = {
         method: 'GET',
         headers: {
@@ -135,42 +140,50 @@ function resultFetchData(searchValue, page) {
         },
     }
 
-    fetch(
-        `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
-            searchValue
-        )}&include_adult=false&language=en-US&page=${page}`,
-        options
-    )
-        .then((response) => response.json())
-        .then((response) => {
-            const filtered_response = response.results.filter((movie) => {
-                if (movie.hasOwnProperty('poster_path')) {
-                    // Now check for non-empty string and not null/undefined
-                    return (
-                        movie.poster_path !== null && movie.poster_path !== ''
-                    )
-                }
-                // If the field doesn't exist, automatically exclude the movie
-                return false
-            })
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+                searchValue
+            )}&include_adult=false&language=en-US&page=${page}`,
+            options
+        )
 
-            resultsPagination.totalPage = response.total_pages
-            resultsPagination.lastSearchInput = searchValue
-            resultsPagination.actualPage = page
-            resultsPagination.totalCount = response.total_results
-            if (page == 1) {
-                console.log(
-                    'first time loading images for the same search input'
-                )
-                initResultImage(filtered_response)
-            } else {
-                console.log(
-                    'updating and loading more images after reaching end of swiper'
-                )
-                updateResultImage(filtered_response)
+        const responseJson = await response.json()
+
+        const filtered_response = responseJson.results.filter((movie) => {
+            if (movie.hasOwnProperty('poster_path')) {
+                return movie.poster_path !== null && movie.poster_path !== ''
             }
+
+            return false
         })
-        .catch((err) => console.error(err))
+
+        resultsPagination.totalPage = responseJson.total_pages
+        console.log(
+            `total number of pages is equal to ${responseJson.total_pages}`
+        )
+        resultsPagination.actualPage = page
+
+        if (page === 1) {
+            console.log(
+                'first time loading images for the this search input ' +
+                    searchValue
+            )
+            console.log(filtered_response)
+            initResultImage(filtered_response)
+        } else {
+            console.log(
+                'updating and loading more images after reaching end of swiper , page : ' +
+                    page
+            )
+            updateResultImage(filtered_response)
+        }
+        resultsPagination.lastSearchInput = searchValue
+
+        console.log(JSON.stringify(resultsPagination))
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 //Main Code
